f6892b3732fda2fecbfede0d10e7f20a181e94e1
mdharris2003 <mdharrisharris@gmail.com>
2026-08-23 12:13:37 +0530

    feat: add flight controller endpoints

---
+++ src/main/java/com/flightmanagement/flight_management_system/controller/FlightController.java
@@ -0,0 +1,40 @@
+package com.flightmanagement.flight_management_system.controller;
+
+import com.flightmanagement.flight_management_system.dto.FlightRequest;
+import com.flightmanagement.flight_management_system.dto.FlightResponse;
+import com.flightmanagement.flight_management_system.service.FlightService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/flights")
+public class FlightController {
+    private final FlightService flightService;
+
+    public FlightController(FlightService flightService){
+        this.flightService = flightService;
+    }
+
+    @GetMapping
+    public List<FlightResponse> getAllFlights(){
+        return flightService.getAllFlights();
+    }
+
+    @GetMapping("/{id}")
+    public FlightResponse getFlightById(@PathVariable Long id){
+        return flightService.getFlightById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<FlightResponse> createFlight(@Valid @RequestBody FlightRequest request){
+        FlightResponse response = flightService.createFlight(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+
+}
